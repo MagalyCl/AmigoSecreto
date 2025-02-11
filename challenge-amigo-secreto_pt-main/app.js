30c07e48887cf622c6c1a3b99b2d8e1167015a71
MagalyCl <claraescola02@gmail.com>
2025-02-11 12:51:29 -0300

    Função adicionar amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,25 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Array que irá armazenar nomes
+let amigos = [];
+
+//Função para adicionar amigos
+
+function adicionarAmigo() {
+    let adicionarNomes = document.querySelector('input');
+    let nome = adicionarNomes.value;
+
+
+if(nome == '') {
+    alert ('O campo está vazio! Por favor insira um nome.');
+} else {
+    amigos.push(nome);
+}
+
+limparCampo();
+
+
+}
+function limparCampo() {
+   let adicionarNomes = document.querySelector('input');
+    adicionarNomes.value = '';
+}
+
