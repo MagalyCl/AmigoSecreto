7841cfb89ce33cf3f8cb088ac03b00dff5b6d2ff
MagalyCl <claraescola02@gmail.com>
2025-02-13 17:16:09 -0300

    Adicionando função para atualizar a lista

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -5,8 +5,7 @@ let amigos = [];
 
 function adicionarAmigo() {
     let adicionarNomes = document.querySelector('input');
-    let nome = adicionarNomes.value;
-
+    let nome = adicionarNomes.value.trim();
 
 if(nome == '') {
     alert ('O campo está vazio! Por favor insira um nome.');
@@ -15,9 +14,23 @@ if(nome == '') {
 }
 
 limparCampo();
-
-
+atualizarLista();
 }
+
+//Função para Atualizar Lista
+
+function atualizarLista() {
+    let lista = document.getElementById ('listaAmigos');
+    lista.innerHTML = '';
+
+    for(nomeAmigo of amigos) {
+        let escolhido = document.createElement('li');
+        escolhido.innerHTML = nomeAmigo;
+        lista.appendChild(escolhido);
+    }
+}
+
+//Função para limpar o campo
 function limparCampo() {
    let adicionarNomes = document.querySelector('input');
     adicionarNomes.value = '';
